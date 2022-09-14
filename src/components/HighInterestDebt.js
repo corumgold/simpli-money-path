4845eba0643545432a264215e349bfc9c58c7e39
Corum Gold <goldcorum@gmail.com>
2022-09-14 16:41:21 -0400

    Clean up high interest debt

--- src/components/HighInterestDebt.js
+++ src/components/HighInterestDebt.js
@@ -13,36 +13,37 @@ const HighInterestDebt = () => {
   };
 
   if (!user.debt) {
+    navigate("/emergency-fund");
     return (
       <>
-        <h2>Since you don't have any debt, you can skip this part!</h2>
-        <button onClick={handleClick}>Move Forward</button>
+        <h1>Since you don't have any debt, you can skip this part!</h1>
+        <button onClick={handleClick}>Move On</button>
       </>
     );
   }
 
   return (
     <>
+      <h1>Let's pay off some debt, {user.name}! </h1>
       <h2>
-        Let's start paying down some debt! Paying off debt not only gives you
-        room to "breathe," but it also frees up extra cash every month!
+        Paying off debt not only gives you room to "breathe" - it also frees up
+        extra cash!
       </h2>
       <h3>
         Of the {formatter.format(user.debt)} you currently owe, take a few
-        minutes to figure out how much of this is HIGH interest debt ({">"}{" "}
+        minutes to figure out how much is HIGH interest debt ({">"}{" "}
         10%).
       </h3>
       <h3>
-        If you don't have any high interest debt, great! You can keep moving,
-        however if so, we recommend using{" "}
+        We recommend using{" "}
         <a href="https://unbury.me/" target={"_blank"} rel="noreferrer">
           this tool
         </a>{" "}
-        to start contributing the{" "}
-        {formatter.format(user.monthlyIncome - user.monthlyExpenses)} you have
-        left over each month to paying off this pesky debt!
+        to help put the{" "}
+        {formatter.format(user.monthlyIncome - user.monthlyExpenses)} left over
+        each month to work and get that debt paid!
       </h3>
-      <button onClick={handleClick}>Move Forward</button>
+      <button onClick={handleClick}>High Interest Debt? Paid!</button>
     </>
   );
 };
