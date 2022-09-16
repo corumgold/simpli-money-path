017e678397c4b97830cbfc994330d5f2e724e226
Corum Gold <goldcorum@gmail.com>
2022-09-16 16:15:59 -0400

    Scroll to top on every route refresh

--- src/App.js
+++ src/App.js
@@ -15,32 +15,38 @@ import {
   Finish,
 } from "./components";
 import Nav from "./components/Nav";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <>
       <Nav />
       <div id="main-content">
-        <Routes>
-          <Route index path="/" element={<Start />} />
-          <Route path="/income" element={<Income />} />
-          <Route path="/expenses" element={<Expenses />} />
-          <Route path="/debts" element={<Debts />} />
-          <Route path="/stop" element={<ExOverIn />} />
-          <Route
-            path="/initial-emergency-fund"
-            element={<InitialEmergency />}
-          />
-          <Route path="/retirement-match" element={<RetirementMatch />} />
-          <Route path="/high-interest-debt" element={<HighInterestDebt />} />
-          <Route path="/emergency-fund" element={<Emergency />} />
-          <Route
-            path="/moderate-interest-debt"
-            element={<ModerateInterestDebt />}
-          />
-          <Route path="/retirement" element={<Retirement />} />
-          <Route path="/finish" element={<Finish />} />
-        </Routes>
+          <ScrollToTop>
+            <Routes>
+              <Route index path="/" element={<Start />} />
+              <Route path="/income" element={<Income />} />
+              <Route path="/expenses" element={<Expenses />} />
+              <Route path="/debts" element={<Debts />} />
+              <Route path="/stop" element={<ExOverIn />} />
+              <Route
+                path="/initial-emergency-fund"
+                element={<InitialEmergency />}
+              />
+              <Route path="/retirement-match" element={<RetirementMatch />} />
+              <Route
+                path="/high-interest-debt"
+                element={<HighInterestDebt />}
+              />
+              <Route path="/emergency-fund" element={<Emergency />} />
+              <Route
+                path="/moderate-interest-debt"
+                element={<ModerateInterestDebt />}
+              />
+              <Route path="/retirement" element={<Retirement />} />
+              <Route path="/finish" element={<Finish />} />
+            </Routes>
+          </ScrollToTop>
       </div>
     </>
   );
